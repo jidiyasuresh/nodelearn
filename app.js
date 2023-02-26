c81080fe91efdd818ba1f67c32144fcadf3ba0ed
jidiyasuresh <suresh.b.jidiya91@gmail.com>
2023-02-26 15:37:06 +0530

    changes updated.

---
+++ app.js
@@ -0,0 +1,10 @@
+// export let x = 40;
+// export let y = 30;
+
+module.exports = {
+    x: 40,
+    y: 30,
+    textFun : function () {
+        return "Test function";
+    }
+}
---
+++ index.js
@@ -0,0 +1,20 @@
+/** ------------ simple program run */
+// var a = 40;
+// var b = 30;
+// var c = 20;
+
+// console.log(a+b+c);
+
+
+//** ------------ app file variable import */
+// import {x} from "./app";
+
+// const app = require('./app');
+// console.log(app);
+// console.log(app.textFun);
+
+//** ------------ core modules use */
+
+// const fs = require('fs').writeFileSync;
+
+// fs("test.txt","This file added for testing purpose.");
